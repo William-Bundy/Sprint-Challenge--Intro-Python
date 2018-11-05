aa99c667a7582269d54403c796c8c28dca5c83c9
William Bundy <williambundy@icloud.com>
2018-11-05 09:55:03 -0800

    Finished city challenge

--- src/cityreader.py
+++ src/cityreader.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python3
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, latitude, and longitude.
 
 # TODO
+class City:
+    def __init__(self, name, latitude, longitude):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,37 +22,22 @@
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 
+from os import path
+import csv
 cities = []
 
-# TODO
+csvpath = path.join(path.dirname(__file__), "cities.csv")
+with open(csvpath, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        try:
+            city = City(row["city"], float(row["lat"]), float(row["lng"]))
+            cities.append(city)
+        except ValueError:
+            # just skip cities that we can't deserialize
+            pass
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 
-# TODO
-
-# *** STRETCH GOAL! ***
-#
-# Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Output the cities that fall
-# within this square.
-#
-# Be aware that the user could specify either a lower-left/upper-right pair of
-# coordinates, or an upper-left/lower-right pair of coordinates. Hint: normalize
-# the input data so that it's always one or the other (what is latMin, latMax?)
-# then search for cities.
-#
-# Example I/O:
-#
-# Enter lat1,lon1: 45,-100
-# Enter lat2,lon2: 32,-120
-# Albuquerque: (35.1055,-106.6476)
-# Riverside: (33.9382,-117.3949)
-# San Diego: (32.8312,-117.1225)
-# Los Angeles: (34.114,-118.4068)
-# Las Vegas: (36.2288,-115.2603)
-# Denver: (39.7621,-104.8759)
-# Phoenix: (33.5722,-112.0891)
-# Tucson: (32.1558,-110.8777)
-# Salt Lake City: (40.7774,-111.9301)
-
-# TODO
+for city in cities:
+    print(city.name, city.latitude, city.longitude)
